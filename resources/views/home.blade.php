5758d8718ab6e85dc579e32e4a4c75ee11ad410f
badgrammar <michael.erlangga2210@gmail.com>
2024-08-17 00:57:34 +0700

    sudah jadi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+    <!-- top navbar -->
+    <div class="flex space-x-4 px-12 py-4 border-b">
+        <div class="font-semibold text-xl">Home</div>
+    </div>
+    <!-- body -->
+     <div class="px-12 py-4">
+        <div><span class="font-semibold text-lg">Hello</span> my name is <span class="font-semibold text-lg">Michael!</span></div>
+     </div>
+</body>
+</html>
